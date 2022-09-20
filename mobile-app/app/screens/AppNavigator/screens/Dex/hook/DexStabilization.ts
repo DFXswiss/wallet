7c596c595d1473218efed12c25eeb1ae4e8f563d
ThaBrad <18183189+ThaBrad@users.noreply.github.com>
2022-09-20 03:43:26 +0200

    dev-797 show return stabilisation info

--- mobile-app/app/screens/AppNavigator/screens/Dex/hook/DexStabilization.ts
+++ mobile-app/app/screens/AppNavigator/screens/Dex/hook/DexStabilization.ts
@@ -11,7 +11,7 @@ import { useSelector } from 'react-redux'
 import { RootState } from '@store'
 import { SwapPathPoolPair } from '@defichain/whale-api-client/dist/api/poolpairs'
 
-export type DexStabilizationType = 'direct-dusd-with-fee' | 'composite-dusd-with-fee' | 'none'
+export type DexStabilizationType = 'direct-dusd-with-fee' | 'composite-dusd-with-fee' | 'return-path-with-fee' | 'none'
 
 type DexStabilizationTokenA = OwnedTokenState | undefined
 type DexStabilizationTokenB = TokenState | undefined
@@ -134,8 +134,29 @@ export function useDexStabilization (tokenA: DexStabilizationTokenA, tokenB: Dex
         },
         type: 'EMERGENCY'
       }]
+    } else if (dexStabilizationType === 'return-path-with-fee') {
+      // This info is default for informing beforehand of the returning swap dexFee
+      announcement = [{
+        lang: {
+          en: `When swapping into dToken assets, please note that if you want to exchange them back into crypto tokens (DFI, dBTC, dETH...), stablecoins (dUSDC, dUSDT) or sell them via DFX against FIAT (EUR, USD, FR...) at a later point in time, a DEX stabilization fee of roughly 12% (currently ${fee}%) will apply. In case of a later exchange within the dToken (dUSD, dTSLA, dAMZN, dCOIN ...), the aforementioned DEX stabilization fee does not apply.`,
+          de: `Beim Tausch in dToken-Vermögenswerte beachte bitte, dass bei einem späteren Rücktausch in Krypto-Token (DFI, dBTC, dETH...), Stablecoins (dUSDC, dUSDT) oder einem Verkauf über DFX gegen FIAT (EUR, USD, FR...) eine DEX-Stabilisierungsgebühr von etwa 12% (derzeit ${fee}%) anfällt. Bei einem späteren Umtausch innerhalb des dToken (dUSD, dTSLA, dAMZN, dCOIN ...) entfällt die vorgenannte DEX-Stabilisierungsgebühr.`,
+          'zh-Hans': `当交换到dToken资产时，请注意，如果你想把它们换回加密代币（DFI，dBTC，dETH...），稳定币（dUSDC，dUSDT）或在以后的时间点通过DFX对FIAT（EUR，USD，FR...）出售，将适用大约12%的DEX稳定费（目前为${fee}%）。如果以后在dToken（dUSD, dTSLA, dAMZN, dCOIN ...）内进行兑换，上述的DEX稳定费不适用。`,
+          'zh-Hant': `当交换到dToken资产时，请注意，如果你想把它们换回加密代币（DFI，dBTC，dETH...），稳定币（dUSDC，dUSDT）或在以后的时间点通过DFX对FIAT（EUR，USD，FR...）出售，将适用大约12%的DEX稳定费（目前为${fee}%）。如果以后在dToken（dUSD, dTSLA, dAMZN, dCOIN ...）内进行兑换，上述的DEX稳定费不适用。`,
+          fr: `Lors de l'échange contre des actifs dToken, veuillez noter que si vous souhaitez les échanger à nouveau contre des crypto-tokens (DFI, dBTC, dETH...), des stablecoins (dUSDC, dUSDT) ou les vendre via DFX contre des FIAT (EUR, USD, FR...) ultérieurement, des frais de stabilisation DEX d'environ 12% (actuellement ${fee}%) s'appliqueront. En cas d'échange ultérieur au sein du dToken (dUSD, dTSLA, dAMZN, dCOIN ...), les frais de stabilisation DEX susmentionnés ne s'appliquent pas.`,
+          es: `Cuando cambie a activos dToken, tenga en cuenta que si quiere volver a cambiarlos por cripto tokens (DFI, dBTC, dETH...), stablecoins (dUSDC, dUSDT) o venderlos a través de DFX contra FIAT (EUR, USD, FR...) en un momento posterior, se aplicará una tasa de estabilización DEX de aproximadamente el 12% (actualmente ${fee}%). En caso de un intercambio posterior dentro del dToken (dUSD, dTSLA, dAMZN, dCOIN ...), no se aplica la mencionada tasa de estabilización DEX.`,
+          it: `Quando si scambiano asset in dToken, si noti che se si desidera scambiarli nuovamente in crypto token (DFI, dBTC, dETH...), stablecoin (dUSDC, dUSDT) o venderli tramite DFX contro FIAT (EUR, USD, FR...) in un momento successivo, si applicherà una commissione di stabilizzazione DEX di circa il 12% (attualmente ${fee}%). In caso di scambio successivo all'interno del dToken (dUSD, dTSLA, dAMZN, dCOIN...), la suddetta commissione di stabilizzazione DEX non si applica.`
+        },
+        version: '>=1.16.1',
+        url: {
+          ios: highFeesUrl,
+          android: highFeesUrl,
+          windows: highFeesUrl,
+          web: highFeesUrl,
+          macos: highFeesUrl
+        },
+        type: 'EMERGENCY'
+      }]
     }
-
     return announcement
   }, [])
 
@@ -236,6 +257,22 @@ export function useDexStabilization (tokenA: DexStabilizationTokenA, tokenB: Dex
       }
     }
 
+    // check for direct return path fee info
+    if (
+      bestPath.length === 1 && (
+        (tokenA.displaySymbol === 'DFI' && tokenB.displaySymbol === 'DUSD') ||
+        (tokenA.displaySymbol === 'dUSDT' && tokenB.displaySymbol === 'DUSD') ||
+        (tokenA.displaySymbol === 'dUSDC' && tokenB.displaySymbol === 'DUSD'))) {
+      return {
+        dexStabilizationType: 'return-path-with-fee',
+        pair: {
+          tokenADisplaySymbol: tokenA.displaySymbol,
+          tokenBDisplaySymbol: tokenB.displaySymbol
+        },
+        dexStabilizationFee: getDexStabilizationFee(tokenB.displaySymbol, tokenA.displaySymbol)
+      }
+    }
+
     /*
       Otherwise, check for composite swap
         1. Get index of DUSD-(DFI | USDT | USDC) pair
@@ -248,7 +285,17 @@ export function useDexStabilization (tokenA: DexStabilizationTokenA, tokenB: Dex
       { tokenADisplaySymbol: 'DUSD', tokenBDisplaySymbol: 'dUSDC' }
     ])
 
-    return compositeSwapDexStabilization ?? _dexStabilization
+    const compositeInverted = getCompositeSwapDexStabilization(bestPath, [
+      { tokenADisplaySymbol: 'DFI', tokenBDisplaySymbol: 'DUSD' },
+      { tokenADisplaySymbol: 'dUSDT', tokenBDisplaySymbol: 'DUSD' },
+      { tokenADisplaySymbol: 'dUSDC', tokenBDisplaySymbol: 'DUSD' }
+    ])
+    if (compositeInverted != null) {
+      compositeInverted.dexStabilizationType = 'return-path-with-fee'
+      compositeInverted.dexStabilizationFee = getDexStabilizationFee('DUSD', 'DFI')
+    }
+
+    return compositeSwapDexStabilization ?? compositeInverted ?? _dexStabilization
   }
 
   return {
