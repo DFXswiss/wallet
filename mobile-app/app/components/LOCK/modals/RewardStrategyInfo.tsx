41f84bf52c1fa80f5d6e282e46252c7e4b96ebfe
Krysh <krysh.developer@gmail.com>
2023-02-09 09:14:10 +0100

    [DEV-1131] test change

--- mobile-app/app/components/LOCK/modals/RewardStrategyInfo.tsx
+++ mobile-app/app/components/LOCK/modals/RewardStrategyInfo.tsx
@@ -2,12 +2,15 @@ import { memo } from 'react';
 import * as React from 'react';
 import { tailwind } from '@tailwind';
 import { Text, View } from 'react-native';
+import { translate } from '@translations';
 
 export const RewardStrategyInfo = (): React.MemoExoticComponent<() => JSX.Element> =>
   memo(() => {
     return (
       <View style={tailwind('bg-white px-4 py-2 h-full')}>
-        <Text style={tailwind('text-black text-base font-normal')}>TODO Marketing provide info text</Text>
+        <Text style={tailwind('text-black text-base font-normal')}>
+          {translate('LOCK/LockDashboardScreen', 'TODO explain why there are different rewards')}
+        </Text>
       </View>
     );
   });
