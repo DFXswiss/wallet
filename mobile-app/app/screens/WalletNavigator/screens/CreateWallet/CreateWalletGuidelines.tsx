ef6c20822a26ca0513cef664c3eb5e9099e01bb1
Krysh <krysh.developer@gmail.com>
2023-03-16 15:03:37 +0100

    [DEV-1389] adapted screenflow

--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/CreateWalletGuidelines.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/CreateWalletGuidelines.tsx
@@ -1,168 +1,69 @@
-import { StackScreenProps } from '@react-navigation/stack'
-import { useState } from 'react'
-import * as React from 'react'
-import { TouchableOpacity } from 'react-native'
-import { Switch, View } from '@components/index'
-import { Button } from '@components/Button'
-import { IconName, ThemedIcon, ThemedScrollView, ThemedText } from '@components/themed'
-import { tailwind } from '@tailwind'
-import { translate } from '@translations'
-import { WalletParamList } from '../../WalletNavigator'
-import { openURL } from '@api/linking'
-import SvgSign from '@assets/images/onboarding/x.svg'
+import { StackScreenProps } from '@react-navigation/stack';
+import { useState } from 'react';
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { View } from '@components/index';
+import { Button } from '@components/Button';
+import { ThemedText } from '@components/themed';
+import { tailwind } from '@tailwind';
+import { translate } from '@translations';
+import { WalletParamList } from '../../WalletNavigator';
+import { openURL } from '@api/linking';
+import SvgSign from '@assets/images/onboarding/x.svg';
+import { MnemonicUnprotected } from '@api/wallet';
+import { ThemedCheckbox } from '@components/themed/ThemedCheckbox';
 
-type Props = StackScreenProps<WalletParamList, 'CreateWalletGuidelines'>
+type Props = StackScreenProps<WalletParamList, 'CreateWalletGuidelines'>;
 
-interface GuidelineItem {
-  icon: IconName
-  title: string
-  subtitle: string
-}
+const HARDCODED_PIN_LENGTH = 6;
 
-const guidelines: GuidelineItem[] = [
-  {
-    title: 'Write the words on paper',
-    subtitle: 'Take note of their correct spelling and correct order.',
-    icon: 'create'
-  },
-  {
-    title: 'Secure them in a safe space',
-    subtitle: 'Store them offline and in a safe place.',
-    icon: 'lock'
-  },
-  {
-    title: 'Keep them private',
-    subtitle: 'Don’t share to anyone. Keep the recovery words only to yourself.',
-    icon: 'visibility-off'
+export function CreateWalletGuidelines({ navigation }: Props): JSX.Element {
+  const [isTermsEnabled, setIsTermsEnabled] = useState(false);
+
+  function navigateToPinCreation(): void {
+    navigation.navigate({
+      name: 'PinCreation',
+      params: {
+        pinLength: HARDCODED_PIN_LENGTH,
+        words: MnemonicUnprotected.generateWords(),
+        type: 'create',
+      },
+      merge: true,
+    });
   }
-]
-
-export function CreateWalletGuidelines ({ navigation }: Props): JSX.Element {
-  const [isEnabled, setIsEnabled] = useState(false)
-  const [isTermsEnabled, setIsTermsEnabled] = useState(false)
-
-  const toggleSwitch = (): void => setIsEnabled(prevState => !prevState)
-  const toggleSwitchTerms = (): void => setIsTermsEnabled(prevState => !prevState)
 
   return (
-    <ThemedScrollView
-      dark={tailwind('bg-dfxblue-900')}
-      light={tailwind('bg-white')}
-      style={tailwind('flex-1 p-4 pt-6')}
-    >
-      <SvgSign style={tailwind('self-center mb-4')} />
-      <ThemedText
-        style={tailwind('text-lg font-semibold')}
-      >
-        {translate('screens/Guidelines', 'Creating a wallet')}
-      </ThemedText>
-
-      <ThemedText
-        dark={tailwind('text-dfxgray-400')}
-        light={tailwind('text-dfxgray-500')}
-        style={tailwind('mt-1 text-sm font-medium')}
-      >
-        {translate('screens/Guidelines', 'Before you create a wallet, you will see your 24 recovery words. Keep them private and secure.')}
-      </ThemedText>
-
-      <TouchableOpacity
-        onPress={() => navigation.navigate('RecoveryWordsFaq')}
-        style={tailwind('mb-2')}
-        testID='recovery_words_button'
-      >
-        <ThemedText
-          dark={tailwind('text-dfxred-500')}
-          light={tailwind('text-primary-500')}
-          style={tailwind('font-medium text-sm')}
+    <View style={tailwind('flex-1 p-4 pt-6')}>
+      <View style={tailwind('flex flex-col flex-grow justify-end')}>
+        <TouchableOpacity
+          onPress={async () =>
+            await openURL(translate('screens/Guidelines', 'https://dfx.swiss/en/terms-and-conditions/'))
+          }
+          style={tailwind('mb-2 self-center')}
+          testID="recovery_words_button"
         >
-          {translate('screens/Guidelines', 'Learn more about recovery words')}
-        </ThemedText>
-      </TouchableOpacity>
-
-      {
-        guidelines.map((g, i) => (
-          <View
-            key={i}
-            style={tailwind('flex-row items-center my-3')}
-          >
-            <ThemedIcon
-              iconType='MaterialIcons'
-              name={g.icon}
-              size={32}
-            />
-
-            <View style={tailwind('flex-col flex-auto ml-6')}>
-              <ThemedText style={tailwind('font-medium')}>
-                {translate('screens/Guidelines', g.title)}
-              </ThemedText>
-
-              <ThemedText
-                dark={tailwind('text-dfxgray-400')}
-                light={tailwind('text-dfxgray-500')}
-                numberOfLines={4}
-                style={tailwind('text-sm')}
-              >
-                {translate('screens/Guidelines', g.subtitle)}
-              </ThemedText>
-            </View>
-          </View>
-        ))
-      }
-
-      <View style={tailwind('flex-row items-center my-4')}>
-        <Switch
-          onValueChange={toggleSwitch}
-          testID='guidelines_switch'
-          value={isEnabled}
-        />
-
-        <View style={tailwind('flex-auto ml-4')}>
           <ThemedText
-            style={tailwind('text-sm font-medium')}
+            dark={tailwind('text-dfxred-500')}
+            light={tailwind('text-primary-500')}
+            style={tailwind('font-medium text-sm')}
           >
-            {translate('screens/Guidelines', 'I understand it is my responsibility to keep my recovery words secure, and losing them will result in the irrecoverable loss of my wallet funds.')}
+            {translate('screens/Guidelines', 'View terms and conditions.')}
           </ThemedText>
-        </View>
-      </View>
-
-      <View style={tailwind('flex-row items-center my-4')}>
-        <Switch
-          onValueChange={toggleSwitchTerms}
-          testID='guidelines_switch_terms'
-          value={isTermsEnabled}
+        </TouchableOpacity>
+        <ThemedCheckbox
+          initialValue={isTermsEnabled}
+          onChanged={setIsTermsEnabled}
+          text={translate('screens/Guidelines', 'I have read and agree to the terms and conditions.')}
         />
-
-        <View style={tailwind('flex-auto ml-4')}>
-          <ThemedText
-            style={tailwind('text-sm font-medium')}
-          >
-            {translate('screens/Guidelines', 'I have read and agree to the terms and conditions.')}
-          </ThemedText>
-
-          <TouchableOpacity
-            onPress={async () => await openURL(translate('screens/Guidelines', 'https://dfx.swiss/en/terms-and-conditions/'))}
-            style={tailwind('mb-2')}
-            testID='recovery_words_button'
-          >
-            <ThemedText
-              dark={tailwind('text-dfxred-500')}
-              light={tailwind('text-primary-500')}
-              style={tailwind('font-medium text-sm')}
-            >
-              {translate('screens/Guidelines', 'View terms and conditions.')}
-            </ThemedText>
-          </TouchableOpacity>
-        </View>
+        <Button
+          disabled={!isTermsEnabled}
+          label={translate('screens/Onboarding', 'CREATE A WALLET')}
+          margin="mx-0 mt-3 mb-12"
+          onPress={navigateToPinCreation}
+          testID="create_recovery_words_button"
+          title="create mnemonic words"
+        />
       </View>
-
-      <Button
-        disabled={!(isEnabled && isTermsEnabled)}
-        label={translate('screens/Guidelines', 'SHOW MY 24 RECOVERY WORDS')}
-        margin='mx-0 mt-3 mb-12'
-        onPress={() => navigation.navigate('CreateMnemonicWallet')}
-        testID='create_recovery_words_button'
-        title='create mnemonic words'
-      />
-    </ThemedScrollView>
-  )
+    </View>
+  );
 }
--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PinCreation.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PinCreation.tsx
@@ -1,38 +1,36 @@
-import { NavigationProp, useNavigation } from '@react-navigation/native'
-import { StackScreenProps } from '@react-navigation/stack'
-import { useEffect, useState } from 'react'
-import { View } from '@components/index'
-import { CREATE_STEPS, CreateWalletStepIndicator, RESTORE_STEPS } from '@components/CreateWalletStepIndicator'
-import { PinTextInput } from '@components/PinTextInput'
-import { ThemedScrollView, ThemedText, ThemedTouchableOpacity, ThemedIcon } from '@components/themed'
-import { tailwind } from '@tailwind'
-import { translate } from '@translations'
-import { WalletParamList } from '../../WalletNavigator'
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { useEffect, useState } from 'react';
+import { View } from '@components/index';
+import { CREATE_STEPS, CreateWalletStepIndicator, RESTORE_STEPS } from '@components/CreateWalletStepIndicator';
+import { PinTextInput } from '@components/PinTextInput';
+import { ThemedScrollView, ThemedText, ThemedTouchableOpacity } from '@components/themed';
+import { tailwind } from '@tailwind';
+import { translate } from '@translations';
+import { WalletParamList } from '../../WalletNavigator';
 
-type Props = StackScreenProps<WalletParamList, 'PinCreation'>
+type Props = StackScreenProps<WalletParamList, 'PinCreation'>;
 
-export function PinCreation ({ route }: Props): JSX.Element {
-  const navigation = useNavigation<NavigationProp<WalletParamList>>()
-  const {
-    pinLength,
-    words,
-    type
-  } = route.params
-  const [newPin, setNewPin] = useState('')
+export function PinCreation({ route }: Props): JSX.Element {
+  const navigation = useNavigation<NavigationProp<WalletParamList>>();
+  const { pinLength, words, type } = route.params;
+  const [newPin, setNewPin] = useState('');
   const goToPasscodeFaq = (): void => {
-    navigation.navigate('PasscodeFaq')
-  }
+    navigation.navigate('PasscodeFaq');
+  };
 
   useEffect(() => {
     if (newPin.length !== pinLength) {
-      return
+      return;
     }
 
-    setNewPin('')
+    setNewPin('');
     navigation.navigate({
-      name: 'PinConfirmation', params: { words, pin: newPin, type }, merge: true
-    })
-  }, [newPin])
+      name: 'PinConfirmation',
+      params: { words, pin: newPin, type },
+      merge: true,
+    });
+  }, [newPin]);
 
   return (
     <ThemedScrollView
@@ -40,35 +38,31 @@ export function PinCreation ({ route }: Props): JSX.Element {
       light={tailwind('bg-white')}
       style={tailwind('w-full flex-1 flex-col')}
       contentContainerStyle={tailwind('items-center')}
-      testID='screen_create_pin'
+      testID="screen_create_pin"
     >
       <CreateWalletStepIndicator
-        current={type === 'create' ? 3 : 2}
+        current={1}
         steps={type === 'create' ? CREATE_STEPS : RESTORE_STEPS}
         style={tailwind('py-4 px-1')}
       />
 
-      <View style={tailwind('px-6 py-4 mb-12')}>
-        <ThemedText
-          style={tailwind('text-center font-semibold')}
-        >
+      <View style={tailwind('px-6 py-4 mb-12 flex items-center')}>
+        <ThemedText style={tailwind('text-center font-semibold')}>
           {translate('screens/PinCreation', `Well done! Your wallet is ${type === 'create' ? 'created' : 'restored'}.`)}
+        </ThemedText>
+        <ThemedText light={tailwind('text-gray-500')} dark={tailwind('text-dfxgray-300')} style={tailwind('text-xs')}>
+          {translate('screens/PinCreation', 'Create new passcode')}
         </ThemedText>
       </View>
 
-      <PinTextInput
-        cellCount={6}
-        onChange={setNewPin}
-        testID='pin_input'
-        value={newPin}
-      />
+      <PinTextInput cellCount={6} onChange={setNewPin} testID="pin_input" value={newPin} />
 
       <ThemedTouchableOpacity
         onPress={goToPasscodeFaq}
         light={tailwind('border-0')}
         dark={tailwind('border-0')}
         style={tailwind('w-4/5 pt-6')}
-        testID='passcode_faq_link'
+        testID="passcode_faq_link"
       >
         <ThemedText
           style={tailwind('text-center text-sm font-semibold')}
@@ -78,23 +72,6 @@ export function PinCreation ({ route }: Props): JSX.Element {
           {translate('screens/PinCreation', 'Learn more about passcode')}
         </ThemedText>
       </ThemedTouchableOpacity>
-      <View style={tailwind('pt-12 flex flex-row text-sm font-medium rounded items-center')}>
-        <ThemedIcon
-          light={tailwind('text-gray-600')}
-          dark={tailwind('text-dfxgray-400')}
-          style={tailwind('pr-1')}
-          iconType='MaterialIcons'
-          name='lock'
-          size={15}
-        />
-        <ThemedText
-          light={tailwind('text-gray-500')}
-          dark={tailwind('text-dfxgray-300')}
-          style={tailwind('text-xs')}
-        >
-          {translate('screens/RecoveryWordsScreen', 'Keep your recovery words safe and private.')}
-        </ThemedText>
-      </View>
     </ThemedScrollView>
-  )
+  );
 }
