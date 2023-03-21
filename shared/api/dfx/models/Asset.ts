13947c61fe2fd35f9e920bf3be206ed9a1c44929
Krysh <krysh.developer@gmail.com>
2023-03-21 14:26:01 +0100

    Fix/general (#66)
    * [NO-TASK] fixed icon sizes
    
    * [NO-TASK] csETH as loan token
    
    * [NO-TASK] fixes issue with csETH visualisation
    
    * [NO-TASK] filter assets
    
    * [NO-TASK] fixes issue with dEUROC showing up as dToken
    
    * [DEV-1402] changed text
    
    * [DEV-743] changed USD to USDT
    
    * [NO-TASK] remove pre commit
    # Conflicts:
    #	shared/api/dfx/models/Asset.ts

--- shared/api/dfx/models/Asset.ts
+++ shared/api/dfx/models/Asset.ts
@@ -5,13 +5,21 @@ export enum AssetType {
   DAT = 'DAT',
 }
 
+export enum AssetCategory {
+  POOL_PAIR = 'PoolPair',
+  STOCK = 'Stock',
+  CRYPTO = 'Crypto',
+}
+
 export interface Asset {
   id: number;
   chainId: string;
   type: AssetType;
   name: string;
   dexName: string;
+  displayName: string;
   buyable: boolean;
   sellable: boolean;
   blockchain: Blockchain;
+  category: AssetCategory;
 }
